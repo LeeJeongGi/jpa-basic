a4a6b0aa10fe7a6e2f9999e3f4337062bafa5b72
LeeJeongGi <apple4rhk@naver.com>
2022-03-31 22:35:53 +0900

    feat : 주문한 아이템 조회하는 기능 추가

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -5,22 +5,26 @@ import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class OrderService {
 
-    private final MemberRepository memberRepository;
     private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
     private final ItemRepository itemRepository;
 
     /**
      * 주문
      */
+    @Transactional
     public Long order(Long memberId, Long itemId, int count) {
 
         //엔티티 조회
@@ -49,10 +53,14 @@ public class OrderService {
      */
     @Transactional
     public void cancelOrder(Long orderId) {
-
         //주문 엔티티 조회
         Order order = orderRepository.findOne(orderId);
-
+        //주문 취소
         order.cancel();
+    }
+
+    //검색
+    public List<Order> findOrders(OrderSearch orderSearch) {
+        return orderRepository.findAllByString(orderSearch);
     }
 }
